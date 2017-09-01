27c7bcdc58e85bbb171e5995d8a371942d780570
Anthony.Griggs <Anthony.Griggs1@marist.edu>
2017-09-01 11:59:05 -0400

    Added new function for debug printing

--- hello.py
+++ hello.py
@@ -2,7 +2,22 @@
 # Author: Anthony Griggs
 # Date: 9/1/17
 
+##################################
+# dprint
+# enables or disables debug printing
+# Simple function used by many, many programmers, I take no credit for it WHATSOEVER
+# to enable, simply set bDebugStatements to true!
+##NOTE TO SELF: in Python, first letter to booleans are Capitalized
+bDebugStatements = True
+
+def dprint(message):
+    if (bDebugStatements == True):
+        print(message)
+
+
+#Simple function with no specific purpose
 def main():
+    dprint("This message is for debugging purposes")
     print("Hello instructor!")
     print("Good bye!")
 main()
