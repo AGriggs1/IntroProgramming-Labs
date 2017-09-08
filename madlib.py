f401b8cdbbb9f2f1dd99cb4f3d0117925a5b5479
Anthony Griggs <Anthony.Griggs1@marist.edu>
2017-09-08 12:05:07 -0400

    Experimenting, if that's fine.

--- madlib.py
+++ madlib.py
@@ -5,16 +5,22 @@
 def main():
     #Hey now hey now hey now
 
- #   i = eval(input("1 or 2?"))
+    #Hm, eval(input()) doesn't seem to like strings
+    i = input("1 or 2? ")
+    if not(str(i) == i):
+        eval(input(i))
     nou = input("Enter a noun: ")
     ver = input("Enter a verb: ")
     adj = input("Enter an adjective: " )
     pla = input("Enter a place: ")
 
- #   i = i or 0
- #   if (i == 0):
- #       print("You think you're smart ya", adj, nou, 
-    print("The", adj, nou, "likes to", ver, "at the", pla, "by the by.")
+    #This I hoped would catch none digits, if it worked, that's what I wanted to see, but uh, see above complaint.
+    #That's what it is. WAH! WAH! Python no WORK!
+    #i = float(i) or 0 #Darn. This doesn't seem to work. Such a shame :(
+    if (i == 0):
+        print("You think you're smart ya", adj, nou, "go", ver, "off in yur", pla + ". Bleh!")
+    else:    
+        print("The", adj, nou, "likes to", ver, "at the", pla, "by the by.")
     print("Go home!")
 main()
     
