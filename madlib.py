fcf44b16aed3bf1392264176d40a110af0ea8499
Anthony Griggs <Anthony.Griggs1@marist.edu>
2017-09-08 11:50:16 -0400

    Solution to Mad Libs exercise

---
+++ madlib.py
@@ -0,0 +1,21 @@
+# Intro to Programming
+# Author: Anthony Griggs
+# Date: 9/8/1
+
+def main():
+    #Hey now hey now hey now
+
+ #   i = eval(input("1 or 2?"))
+    nou = input("Enter a noun: ")
+    ver = input("Enter a verb: ")
+    adj = input("Enter an adjective: " )
+    pla = input("Enter a place: ")
+
+ #   i = i or 0
+ #   if (i == 0):
+ #       print("You think you're smart ya", adj, nou, 
+    print("The", adj, nou, "likes to", ver, "at the", pla, "by the by.")
+    print("Go home!")
+main()
+    
+    
