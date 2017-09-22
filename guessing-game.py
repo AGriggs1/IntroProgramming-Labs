1fe5a571271052f44f87a51f79e3ee502783f028
Anthony Griggs <Anthony.Griggs1@marist.edu>
2017-09-22 11:29:50 -0400

    Usability improvements to the guessing game, resolves #3 and resolves #4

--- guessing-game.py
+++ guessing-game.py
@@ -9,10 +9,17 @@ def main():
     print("I am thinking of an animal.")
     while (bGuess == False):
         sGuess = input("What animal might it be?\n")
+        sGuess = sGuess.capitalize()
+      #  print(sGuess)
         if (sGuess == sAnimal):
             print("I'M A GIRAFFE!\n"
                   "You win.")
             bGuess = True
+        elif (sGuess == "Quit"): #WHAT? elif?! Gh.
+            print("Oi, lookie here, another one bites the dust.\n"
+                  "You lose.")
+            break #You don't even get the satisfaction of bGuess == True!
         else:
             print("Nah. Try again.")
     
+main()
