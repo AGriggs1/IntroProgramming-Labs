d1e9a909677fe83184bb7d625ee4e7dc5f2fae36
Anthony Griggs <Anthony.Griggs1@marist.edu>
2017-09-29 11:29:09 -0400

    Improved credentials program. Split main into three functions, one that gets the user's full name, one that creates the username, and one that gets the user's password. main now also requires parameters, unlike the previous version.

--- credentials.py
+++ credentials.py
@@ -2,18 +2,35 @@
 # Lab #5 – Working with Strings and Functions
 # Author: Anthony Griggs
 # Created: 2017-09-29
-def main():
+
+def getFullName():
+    fullName = []
     # get user's first and last names
     first = input("Enter your first name: ")
-    first = first + "."
+    fullName.append(first)
+    #first = first + "."
     last = input("Enter your last name: ")
-    uname = first + last
-    # ask user to create a new password
+    fullName.append(last)
+    return fullName #Should fullName store first and last names separately?
+
+def concatnateName():
+    fullName = getFullName() #Fetch the list
+    fullName = ".".join(fullName) #Join the names in the list together, replacing the list with a string
+    return fullName #Return the list
+
+def getPassword(passLength):
     passwd = input("Create a new password: ")
-    while len(passwd) < 8:
+    while len(passwd) < passLength:
         print("Fool of a Took! That password is feeble!")
         passwd = input("Create a new password: ")
+    return passwd
+
+def main(uname, passwd):
     print("The force is strong in this one...")
     print("Account configured. Your new email address is",
     uname + "@marist.edu")
-main()
+
+uname = concatnateName()
+passwd = getPassword(8)
+main(uname, passwd)
+
