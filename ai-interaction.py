876c094582040eec2214d7ff040ccd69e83cf59a
Anthony Griggs <Anthony.Griggs1@marist.edu>
2017-11-03 11:26:04 -0400

    Initial commit. Basically maps out plan for emotion matrix at this point.

---
+++ ai-interaction.py
@@ -0,0 +1,53 @@
+# Intro to Programming
+# Author: Anthony Griggs
+# Date: 11/3 2017
+
+#emotions: anger, disgust, fear, happiness, sadness, surprise
+#actiones: reward, punish, threaten, joke
+
+#anger > (reward)happiness
+#anger > (punish)fear
+#anger > (threaten)anger
+#anger > (joke)digust
+
+#happiness > (reward)happiness
+#happiness > (punish)sadness
+#happiness > (threaten)disgust
+#happiness > (joke)sadness
+
+#disgust > (reward)disgust
+#disgust > (punish)disgust
+#disgust > (threaten)surprise
+#disgust > (joke)disgust
+
+#fear > (reward)disgust
+#fear > (punish)fear
+#fear > (threaten)anger
+#fear > (joke)surprise
+
+#sadness > (reward)happiness
+#sadness > (punish)sadness
+#sadness > (threaten)sadness
+#sadness > (joke)anger
+
+#surprise > (reward)fear
+#surprise > (punish)anger
+#surprise > (threaten)anger
+#surprise > (joke)sadness
+
+def Main():
+    print("Initializing...")
+    print("Welcome! I am Ax900012x-y, A generation IV class-Y AI. I am tasked to oversee the human interactions and machine learning techniques of other AIs.")
+    print("Let us begin. Today we will be working with Kx110023x-b")
+    pEmotion = None #Let's pretend AIs are objects
+    while True:
+            #get input
+            #fire SetInteraction
+            #set emotion
+            #repeat 
+        pass
+
+def SetInteraction(sInput, pEmotions):
+    #check sInput
+    #set emotion, based on pEmotion
+    pass
