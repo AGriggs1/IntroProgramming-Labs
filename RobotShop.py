bed9527514597f228ecc773fa6e42858db2dab33
Anthony Griggs <Anthony.Griggs1@marist.edu>
2017-11-17 11:49:33 -0500

    Reworked printStock function to operate with the class.

--- RobotShop.py
+++ RobotShop.py
@@ -2,12 +2,7 @@
 #Author: Anthony Griggs
 #11/17/2017
 
-productNames = [ "Ultrasonic range finder",
-                 "Servo motor",
-                 "Microcontroller Board",
-                 "Laser range finder",
-                 "Lithium polymer battery"
-                 ]
+
 ##===========================================================================================
 class Product:
     def __init__(self, name, price, stock):
@@ -25,22 +20,28 @@ class Product:
        #Make sure there is enough in stock to take away before taking away the defined amount
        if self.Stock - qty > 0: self.Stock -= qty
 ##===========================================================================================
-productPrices = [ 2.50, 14.99, 44.95, 34.95, 149.99, 8.99 ]
-productQuantities = [ 4, 10, 5, 7, 2, 8 ]
 
+products = []
 ultrasonic = Product("Ultrasonic ranger finder", 2.50, 4)
-servo = Product("Servo motor" 14.99, 10)
+products.append(ultrasonic)
+servo = Product("Servo motor", 14.99, 10)
+products.append(servo)
 microcont = Product("Microcontroller Board", 44.95, 5)
+products.append(microcont)
 laser = Product("Laser range finder", 149.99, 7)
+products.append(laser)
 lithium = Product("Lithium polymer battery", 8.99, 8)
+products.append(lithium)
 
 def printStock():
     print()
     print("Available Products")
     print("------------------")
-    for i in range(0, len(productNames)):
-        if productQuantities[i] > 0:
-            print(str(i)+")", productNames[i], "$", productPrices[i])
+    i = 0
+    for p in products:
+        if p.isInStock: print(str(i) + ")", p.name, "$", p.price)
+           # print(str(i)+")", productNames[i], "$", productPrices[i])
+        i += 1
     print()
 
 def main():
